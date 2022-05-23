3378eb86211ec3a1afcdc5ca0ebf7f411fb80305
Salif KEITA <you@example.com>
2022-05-23 09:46:13 +0000

    le premier commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wordle_clone/widgets/wordle_page.dart';
+
+void main() {
+  const providerScopedApp = ProviderScope(child: MyApp());
+  FlutterError.onError = (FlutterErrorDetails details) {};
+  runApp(providerScopedApp);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const WordlePage(),
+    );
+  }
+}
